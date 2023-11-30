aabdef9eaff6df9a81a62b75339430f010c1a80a
Peter Rasser <peter.rasser@hakzell.at>
2023-11-30 20:59:53 +0100

    bugfix: remove dollar sign

--- Program.cs
+++ Program.cs
@@ -3,4 +3,4 @@
 Rechteck r1 = new Rechteck(2 ,3);
 double fläche = r1.GetFläche();
 
-Console.WriteLine($"Der Flächeninhalt beträgt: {0}", fläche);
+Console.WriteLine("Der Flächeninhalt beträgt: {0}", fläche);
